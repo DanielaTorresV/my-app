55839997881f14e37beebce85860a676b6b905d3
Daniela <dannytorres0211@gmail.com>
2022-05-09 12:28:22 -0500

    Modified App.js import components from components folder and include components folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
+import React from "react";
 import './App.css';
+import Title from "./components/Title.js";
+import Subtitle from "./components/Subtitle.js";
+import Button from "./components/Button.js";
 
 function App() {
   return (
     <div className="App">
      <Title title="Está es mi primera interacción con React"/>
-     <Subtitle text="Esté es el subtítulo para mi primera interacción con Reacts"/>
+     <Subtitle text="Esté es el subtítulo para mi primera interacción con React"/>
      <Button text="Mi 1er Botón en React"/>
     </div>
   );
-};
-
-const Title = (props) => {
-  return <h1 className="Title">{props.title}</h1>
-};
-
-const Subtitle = (props) => {
-  return <h4>{props.text}</h4>
-};
-
-const Button = (props) => {
-return <button className="Button">{props.text}</button>
 };
 
 export default App;
